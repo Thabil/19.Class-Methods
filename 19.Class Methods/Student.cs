8367ef97505b24ef0c48f01e38eeabee874c2faa
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:58:44 +0200

    Add project files.

---
+++ 19.Class Methods/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Class_Methods
+{
+    internal class Student
+    {
+
+        public double weight;
+        public string name;
+        public int age;
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;//the word this is an explict way of making the allocation.
+            this.name = aName;
+            this.age = aAge; //You now have one stop shop to test Obesity of your students and can update the info as you go
+        }
+
+
+        public bool IsObese()
+        {
+            if (weight > 60)
+                return true;
+            else return false;
+
+        }
+
+    }    
+}
